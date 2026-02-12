2bccadc4584e3720558e1c6d2fdc8ed10829d177
Geovane Silva Prazeres <geovanesil45sil@gmail.com>
2026-02-12 15:21:36 -0300

    ex

---
+++ src/LoadingErroEx/LoadingErroEx2.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoadingErroEx2 = () => {
+  return (
+    <div>
+        <h2>🟢 Exercício 2 — Error Simulado</h2>
+    </div>
+  )
+}
+
+export default LoadingErroEx2
